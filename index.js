689206c88aaf57bcc752797a017b282810d265c7
Gus Suarez <gus@Guss-MacBook-Pro.local>
2016-02-03 00:07:32 -0800

    first commit, checklist completed

---
+++ index.js
@@ -0,0 +1,43 @@
+var path = require('path');
+
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
+app.use('/code/challenge', function (req, res, next) {
+
+	res.status(201);
+
+	var responseJson = {
+		email: 'gus@mythril.co',
+		name: {
+			first: 'Gus',
+			last: 'Suarez'
+		}, 
+		website: 'mythril.co',
+		github_repo_link: 'https://github.com/aerze'
+	};
+
+
+	res.json(responseJson);
+
+});
+
+
+var server = app.listen(8080, function () {
+	console.log('Your server is running on port 8080');
+});
